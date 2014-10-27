ddeaa4206b349ee945a9b42a6e363553a141f135
OpenBijbel.nu Ontwikkelaar <Openbijbel.nu@gmail.com>
2014-10-27 19:50:24 +0100

    Update Mosterdzaad.js

--- Mosterdzaad.js
+++ Mosterdzaad.js
@@ -34,7 +34,7 @@
                           "date": "27-10-2014",
                           "check": "None",
                           "check-date": "27-10-2014"
-              }
+              },
               "translit": "Kago elthon pros hymas adelphoi elthon ou kath' hyperochen logou e sophias katangellon hymin to martyrion tou Theou",
               "strongs": "",
               "meaning": "And I - having come - to - you - brothers - came - not - according to - excellency - of speech - or - wisdom - proclaiming - to you - the -testimony - - of God",
