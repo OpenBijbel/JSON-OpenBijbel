d103bc514fcc10d16f3b63a083a4246b3d43c962
OpenBijbel.nu Ontwikkelaar <Openbijbel.nu@gmail.com>
2014-10-27 19:47:06 +0100

    Update toon-Mosterdzaad.js

--- toon-Mosterdzaad.js
+++ toon-Mosterdzaad.js
@@ -1,14 +1,7 @@
 function getVerse() {
   $.getJSON( "https://raw.githubusercontent.com/OpenBijbel/JSON-OpenBijbel/master/Mosterdzaad.js", function( versedata ) {
-      var theVerseData = versedata;
-      alert(theVerseData);
-       var verseText = versedata.text;
-       var verseTranslator = versedata.meta.translator;
-      alert(verseTranslator);
-      alert(verseText);
+      alert(verseData);
   });
 };
-
-desiredVerse = "1Cor2.1";
 
 getVerse();
